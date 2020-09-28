6c2677b145385169c4820223b96786ddd4696e44
danil milostiviy <milostiviy.danil@yandex.ru>
2020-09-28 12:41:38 +0300

    add saga

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import TextInput from './TextInput';
-import { DEBOUNCE_TIME, MY_API_KEY } from '../const';
-import { useDebounce , fetchData } from '../utils';
+import { useDispatch } from "react-redux";
+import { ActionCreator } from '../redux/reducer';
 
-const getFilms = async (searchStr, page = 1) => {
-  const FilmsData = await fetchData(`http://www.omdbapi.com/?apikey=${MY_API_KEY}&s=${searchStr}&page=${page}`);
-  console.log(FilmsData);
-  return FilmsData;
-};
+import TextInput from './TextInput';
+
+import { DEBOUNCE_TIME } from '../const';
+import { useDebounce } from '../utils';
 
 const Search = () => {
+  const dispatch = useDispatch();
   const [searchStr, setSearchStr] = useState('');
 
-  const debouncedString = useDebounce(searchStr, DEBOUNCE_TIME);
+  const debouncedString = useDebounce(searchStr.trim().replace(/\s+/g, ' '), DEBOUNCE_TIME);
 
   const handleChange = evt => {
     setSearchStr(evt.target.value);
@@ -20,7 +19,7 @@ const Search = () => {
 
   useEffect(() => {
     if (debouncedString) {
-      getFilms(debouncedString);
+      dispatch(ActionCreator.setSeatchStr(debouncedString));
     } else {
       console.log('do nothing');
     }
@@ -32,14 +31,5 @@ const Search = () => {
     </>
   );
 };
-
-
-{/* { films.map((film) =>
-  <li key={film.imdbID}>
-    <h4>{film.Title}</h4>
-    <img src={film.Poster} alt=""/>
-    <span>{film.Year}</span>
-  </li>
-)} */}
 
 export default Search;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { sagaWatcher } from './redux/sagas';
+import { reducer } from './redux/reducer';
+
 import App from './components/App';
 
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { Provider } from "react-redux";
-import { reducer } from './redux/reducer.js';
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducer,
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  )
 );
+
+sagaMiddleware.run(sagaWatcher);
 
 ReactDOM.render(
   <Provider store={store}>
