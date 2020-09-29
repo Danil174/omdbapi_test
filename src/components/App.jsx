e8b941c89c2b6decb8889b8450d7c05a79b16eba
danil milostiviy <milostiviy.danil@yandex.ru>
2020-09-29 23:45:50 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Switch, Route } from "react-router-dom";
 import { AppRoutes } from '../const';
 
@@ -10,7 +11,7 @@ const App = () => {
     <Switch>
       <Route path={AppRoutes.ROOT} exact component={MainPage} />
       <Route exact path={`${AppRoutes.FILM_PAGE}/:id?`} component={MoviePage} />
-      <Route render={() => <h1 style={{ color: 'red', textAlign: 'center' }}>404 not found</h1>} />
+      <Route render={() => <h1 style={{ color: 'red', textAlign: 'center' }}>404 not found, move  <Link to={AppRoutes.ROOT}>home</Link></h1>} />
     </Switch>
   );
 };
